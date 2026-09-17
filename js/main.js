c6e57c922dcfce30206a1d7b3b5b8ba54cb4a09f
finwardana <finwardana@gmail.com>
2026-09-17 23:49:57 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 console.log("FINWARDANA Loaded");
-fetch("data.json")
+
+const dataPath =
+window.location.pathname.includes("/games/") ||
+window.location.pathname.includes("/tools/") ||
+window.location.pathname.includes("/invitations/")
+? "../data.json"
+: "data.json";
+
+fetch(dataPath)
 .then(res => res.json())
 .then(data => {
 
@@ -8,6 +16,10 @@ const games = document.getElementById("games-slider");
 const tools = document.getElementById("tools-slider");
 const invitations = document.getElementById("invitations-slider");
 
+const gamesList = document.getElementById("games-list");
+const toolsList = document.getElementById("tools-list");
+const invitationsList = document.getElementById("invitations-list");
+
 function createCard(item){
 
 return `
@@ -15,41 +27,113 @@ return `
 <div class="thumb"
 style="background-image:url('${item.thumb}')">
 </div>
-
 <h3>${item.title}</h3>
 </a>
 `;
 
 }
 
-data.slice(0,10).forEach(item=>{
+/* HOME - RECENTLY ADDED */
+
 if(recent){
+
+data
+.slice(0,10)
+.forEach(item=>{
+
 recent.innerHTML += createCard(item);
-}
+
 });
 
-data
-.filter(item => item.type==="game")
-.forEach(item=>{
+}
+
+/* HOME - GAMES */
+
 if(games){
+
+data
+.filter(item=>item.type==="game")
+.forEach(item=>{
+
 games.innerHTML += createCard(item);
-}
+
 });
 
-data
-.filter(item => item.type==="tool")
-.forEach(item=>{
+}
+
+/* HOME - TOOLS */
+
 if(tools){
-tools.innerHTML += createCard(item);
-}
-});
 
 data
-.filter(item => item.type==="invitation")
+.filter(item=>item.type==="tool")
 .forEach(item=>{
-if(invitations){
-invitations.innerHTML += createCard(item);
-}
+
+tools.innerHTML += createCard(item);
+
 });
+
+}
+
+/* HOME - INVITATIONS */
+
+if(invitations){
+
+data
+.filter(item=>item.type==="invitation")
+.forEach(item=>{
+
+invitations.innerHTML += createCard(item);
+
+});
+
+}
+
+/* PAGE - GAMES */
+
+if(gamesList){
+
+data
+.filter(item=>item.type==="game")
+.forEach(item=>{
+
+gamesList.innerHTML += createCard(item);
+
+});
+
+}
+
+/* PAGE - TOOLS */
+
+if(toolsList){
+
+data
+.filter(item=>item.type==="tool")
+.forEach(item=>{
+
+toolsList.innerHTML += createCard(item);
+
+});
+
+}
+
+/* PAGE - INVITATIONS */
+
+if(invitationsList){
+
+data
+.filter(item=>item.type==="invitation")
+.forEach(item=>{
+
+invitationsList.innerHTML += createCard(item);
+
+});
+
+}
+
+})
+.catch(err=>{
+
+console.error(err);
 
 });
